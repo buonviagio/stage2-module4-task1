604a5629b3743ac4014cb5e0e9fecdca02770f00
Shapovalov Dmytro <buonviagio@gmail.com>
2023-05-11 18:28:58 +0300

    My first commit Stage#2 Module#4 Task#1

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,4 +2,15 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
     // Write your code here!
+    private static volatile ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton (){}
+
+    public static ThreadSafeSingleton getInstance() {
+        ThreadSafeSingleton threadSafeSingleton = instance;
+        if (threadSafeSingleton == null){
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,37 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String appConfigPath = rootPath + "h2database.properties";
+        Connection connection = null;
+
+        try (FileInputStream fio = new FileInputStream(appConfigPath)) {
+            Properties appProps = new Properties();
+            appProps.load(fio);
+
+            Class.forName(appProps.getProperty("jdbc_driver"));
+
+            connection = DriverManager.getConnection(appProps.getProperty("db_url"),
+                    appProps.getProperty("user"),
+                    appProps.getProperty("password"));
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
     // Write your code here!
 }
 
